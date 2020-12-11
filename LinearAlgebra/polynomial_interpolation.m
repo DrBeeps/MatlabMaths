3b67a33bedabc68e42071229f6dfb6b161064f38
Dr. B <boweneveritt@gmail.com>
2020-12-11 11:36:00 -0800

    initial commit

---
+++ LinearAlgebra/polynomial_interpolation.m
@@ -0,0 +1,22 @@
+% there is a built in function that returns coefficients for a polynomial
+% p(x) of degree that is a best fit for the data in y
+
+x = linspace(0, 4*pi, 10); % generate 10 points equally spaceed along a sine curve at interval [0, 4*pi]
+y = sin(x);
+
+poly_test = polyfit(x, y, 7); % fit a 7th degree polynomial to the points
+
+x1 = linspace(0, 4*pi); % fit the to be evaluted polynomial to several places around the curve at interval [0, 4*pi]
+y1 = polyval(poly_test, x1); 
+
+figure
+plot(x, y, 'o')
+hold on
+plot (x1, y1)
+hold off
+
+% time to make our own
+
+% function polyfit2 = poly = polyfit2 (poly)
+%     poly = ;
+% end
---
+++ LinearAlgebra/polynomials.m
@@ -0,0 +1,19 @@
+% polyval() returns the value of a polynomial of degree n evaluated at x
+% the input argument p is a vector of length n+1 whose elements are
+% coefficients in descending powers of the polynomial to be evaluated
+
+poly = [4 0 0 -3 2 33]; % create a vector that represents the polynomial p(x) = 4x^5 - 3x^2 + 2x + 33
+
+% evaluating poly by hand:
+% 4*2^5 - 3*2^2 + 2*2 + 33
+% 128 - 12 + 4 + 33
+% 153 yep polyval() works
+
+
+polyout = polyval(poly, 2); % evaluate polynomial poly at x = 2
+
+disp(polyout)
+
+% function polyval2 poly = polyval2(poly)
+%     poly = 
+% end
